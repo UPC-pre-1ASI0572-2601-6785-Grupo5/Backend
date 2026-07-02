3d2c36416381f779774db58304d1d3c50e95f8ab
cristianipa7 <cristianipa7@gmail.com>
2026-07-02 13:22:51 -0500

    fix: add JsonProperty to truckId in DispatchOrderRequest to fix 400 Bad Request

--- src/main/java/com/fueltrack/platform/orderpayment/interfaces/rest/requests/DispatchOrderRequest.java
+++ src/main/java/com/fueltrack/platform/orderpayment/interfaces/rest/requests/DispatchOrderRequest.java
@@ -1,9 +1,10 @@
 package com.fueltrack.platform.orderpayment.interfaces.rest.requests;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 
 /**
  * Request payload for dispatching an order with an assigned truck.
  */
-public record DispatchOrderRequest(@NotBlank String truckId) {
+public record DispatchOrderRequest(@JsonProperty("truckId") @NotBlank String truckId) {
 }
